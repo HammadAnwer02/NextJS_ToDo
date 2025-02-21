a4adb1496a59ee5e2721d53b030dad194e202076
HammadAnwer02 <hammadanwer76@gmail.com>
2025-02-21 13:50:26 +0500

    Made some fixes for vercel deployement

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -6,7 +6,7 @@ import { authOptions } from "@/lib/authOptions";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
-  if (!session) {
+  if (!session || !session.user || !session.user.id) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
   
